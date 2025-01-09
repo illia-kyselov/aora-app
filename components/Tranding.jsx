cf2246a86cbc0d87fef1281798c328595ecf851b
_PaLa4_ <illya.kis99@gmail.com>
2025-01-09 19:04:18 +0200

    first commit

---
+++ components/Tranding.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+
+import { icons } from "../constants";
+import { useVideoPlayer, VideoView } from "expo-video";
+
+const VideoCard = ({ title, creator, avatar, thumbnail, video }) => {
+    const [play, setPlay] = useState(false);
+
+    const player = useVideoPlayer(video, player => {
+        player.loop = true;
+        player.play();
+    });
+
+    return (
+        <View className="flex flex-col items-center px-4 mb-14">
+            <View className="flex flex-row gap-3 items-start">
+                <View className="flex justify-center items-center flex-row flex-1">
+                    <View className="w-[46px] h-[46px] rounded-lg border border-secondary flex justify-center items-center p-0.5">
+                        <Image
+                            source={{ uri: avatar }}
+                            className="w-full h-full rounded-lg"
+                            resizeMode="cover"
+                        />
+                    </View>
+
+                    <View className="flex justify-center flex-1 ml-3 gap-y-1">
+                        <Text
+                            className="font-psemibold text-sm text-white"
+                            numberOfLines={1}
+                        >
+                            {title}
+                        </Text>
+                        <Text
+                            className="text-xs text-gray-100 font-pregular"
+                            numberOfLines={1}
+                        >
+                            {creator}
+                        </Text>
+                    </View>
+                </View>
+
+                <View className="pt-2">
+                    <Image source={icons.menu} className="w-5 h-5" resizeMode="contain" />
+                </View>
+            </View>
+
+            {play ? (
+                <VideoView
+                    style={{ width: '100%', height: 240, borderRadius: 12 }}
+                    player={player}
+                    allowsFullscreen
+                    allowsPictureInPicture
+                    nativeControls
+                />
+            ) : (
+                <TouchableOpacity
+                    activeOpacity={0.7}
+                    onPress={() => setPlay(true)}
+                    className="w-full h-60 rounded-xl mt-3 relative flex justify-center items-center"
+                >
+                    <Image
+                        source={{ uri: thumbnail }}
+                        className="w-full h-full rounded-xl mt-3"
+                        resizeMode="cover"
+                    />
+
+                    <Image
+                        source={icons.play}
+                        className="w-12 h-12 absolute"
+                        resizeMode="contain"
+                    />
+                </TouchableOpacity>
+            )}
+        </View>
+    );
+};
+
+export default VideoCard;
---
+++ components/index.js
@@ -0,0 +1,17 @@
+import VideoCard from "./VideoCard";
+import FormField from "./FormField";
+import CustomButton from "./CustomButton";
+import InfoBox from "./InfoBox";
+import Tranding from "./Tranding";
+import SearchInput from "./SearchInput";
+import EmptyState from "./EmptyState";
+
+export {
+    VideoCard,
+    FormField,
+    CustomButton,
+    InfoBox,
+    Tranding,
+    SearchInput,
+    EmptyState,
+};
